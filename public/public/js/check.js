7acfc59e8dd6abc7aab0fa0bd93353c91f22956e
xxl <2427018614@qq.com>
2020-11-18 15:28:06 +0800

    xxl_update

--- public/public/js/check.js
+++
@@ -1,7 +0,0 @@
-
-        var userToken = window.localStorage.getItem("yq_token");
-        console.log(userToken)
-       if(userToken!=0&&userToken!=null){
-           alert("您已登录，为您自动跳转")
-            window.location.href = "test-home-page.html"    
-       }
--- public/public/js/cx_d.js
+++
@@ -1,6 +0,0 @@
-window.onload=function(){
-    tijiao=function(){
-     alert($(".cx_form").html)
-    }
-
-}
